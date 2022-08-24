b9f5d224592d3021c95689f6e265774486659b2e
Michał Purchla <michalpurchla@gmail.com>
2022-08-24 17:56:22 +0200

    Test

--- food.py
+++ food.py
@@ -1,18 +1,18 @@
 from turtle import Turtle
 from random import randint
 
+
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
         self.shape('circle')
         self.penup()
-        self.shapesize(stretch_len=0.7, stretch_wid=0.5)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
-        # self.refresh()
+        self.refresh()
 
     def refresh(self):
         """New food random position"""
-        self.goto(x=randint(-280, 280), y=randint(-280,280))
-
+        self.goto(x=randint(-280, 280), y=randint(-280, 280))
